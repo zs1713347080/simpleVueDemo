aea9a57de28a3389ca69f257396534e82d46849c
zs1713347080 <1713347080@qq.com>
2020-04-12 12:33:03 +0800

    next-Tick完善

--- 添加了类组件和完整生命周期的版本/source/vue/util/nextTick.js
+++ 添加了类组件和完整生命周期的版本/source/vue/util/nextTick.js
@@ -1,26 +1,36 @@
-let callbacks = [];
-function flushCallbacks(){
-    callbacks.forEach(cb=>cb())
+let callbacks = []                      //需要在nextTick执行的回调
+let pending = false                     //是否正在pending中
+let timerFunc
+function flushCallbacks(){              //清空回调函数队列的函数
+    pending = false
+    callbacks.forEach(cb=>cb());
     callbacks = []
 }
-export function nextTick(cb){
-    callbacks.push(cb)
-    let asyncFn = () =>{
-        flushCallbacks();
+export function nextTick(cb) {
+    callbacks.push(cb)                  //把回调放入回调队列中
+    if(!pending) {                      //如果不是pending状态就开启一个微任务
+        pending = true
+        timerFunc()
     }
+}
 
-    if(Promise){
-        return Promise.resolve().then(asyncFn)
+if(Promise) {                                           //先进行浏览器能力判断，决定使用哪一种异步
+    timerFunc = () => { 
+        Promise.resolve().then(flushCallbacks)
     }
-    if(MutationObserver){
-        let observe = new MutationObserver(asyncFn);
-        let textNode = document.createTextNode(1);
-        observe.observe(textNode);
+} else if (MutationObserver) {
+    let observe = new MutationObserver(flushCallbacks);
+    let textNode = document.createTextNode(1);
+    observe.observe(textNode,{characterData:true});
+    timerFunc = () => {
         textNode.textContent = 2;
-        return 
     }
-    if(setImmediate){
-        return setImmediate(asyncFn)
+} else if (setImmediate) {
+    timerFunc = () => {
+        setImmediate(flushCallbacks);
     }
-    setTimeout(asyncFn,0)
+} else {
+    timerFunc = () => {
+        setTimeout(flushCallbacks, 0)
+    }
 }
